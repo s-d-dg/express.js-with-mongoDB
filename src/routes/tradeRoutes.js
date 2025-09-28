06d8a7c57df1fdfda8152a906405698359c9edf6
Damian Gasiorowski <softwaredevelopment.dg@gmail.com>
2025-09-28 14:52:08 +0200

    [express] save records in db

--- src/routes/tradeRoutes.js
+++ src/routes/tradeRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { fetchTrades } from "../service/binance.js";
+import { Trade } from "../models/trade.js";
 
 const router = Router();
 
@@ -7,7 +8,34 @@ router.get("/", async (req, res) => {
   const { symbol, limit } = req.query;
   console.log("symbol, limit", symbol, limit);
   const trades = await fetchTrades(symbol, limit || 500);
-  //   const trades = await Trade.find({ symbol: req.params.symbol.toUpperCase() });
+
+  const latestSavedTradeDatetime = await Trade.findOne({
+    symbol: symbol.toUpperCase(),
+  })
+    .sort({ timestamp: -1 })
+    .select("timestamp")
+    .lean();
+
+  const tradesToSave = trades.data
+    .filter((trade) =>
+      latestSavedTradeDatetime
+        ? new Date(trade.time) > latestSavedTradeDatetime
+        : true
+    )
+    .map((trade) => ({
+      id: trade.id,
+      symbol: symbol.toUpperCase(),
+      price: parseFloat(trade.price),
+      quantity: parseFloat(trade.qty),
+      quoteQty: parseFloat(trade.quoteQty),
+      timestamp: new Date(trade.time),
+      isBuyerMaker: trade.isBuyerMaker,
+      isBestMatch: trade.isBestMatch,
+    }));
+
+  if (tradesToSave.length > 0) {
+    await Trade.insertMany(tradesToSave);
+  }
 
   res.json({
     symbol,
